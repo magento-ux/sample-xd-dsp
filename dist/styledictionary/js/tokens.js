121c6c1a17714ff2b68e49a62841f7436582ed1b
Eugene Bannykh <ybannykh@adobe.com>
2020-10-22 11:53:49 -0500

    first commit

---
+++ dist/styledictionary/js/tokens.js
@@ -0,0 +1,13 @@
+/**
+ * Do not edit directly
+ * Generated on Thu, 22 Oct 2020 16:51:32 GMT
+ */
+
+export const Red = "#ff0f0f";
+export const White = "#ffffff";
+export const ButtonLabelFamily = "Helvetica Neue";
+export const ButtonLabelStyle = "Bold";
+export const TitleFamily = "Helvetica Neue";
+export const TitleStyle = "Regular";
+export const ButtonLabelSize = "16rem";
+export const TitleSize = "20rem";
